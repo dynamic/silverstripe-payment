15cdf345f4f59b4c087d134215050ad8f925f5ea
Jeremy <jeremy@burnbright.co.nz>
2011-07-14 21:02:04 -0700

    Removed hard coded html, because it is hard to manager later on.

--- code/types/ChequePayment.php
+++ code/types/ChequePayment.php
@@ -12,7 +12,7 @@ class ChequePayment extends Payment {
 	 */
 	function processPayment($data, $form) {
 		$this->Status = 'Pending';
-		$this->Message = '<p class="warningMessage">' . _t('ChequePayment.MESSAGE', 'Payment accepted via Cheque. Please note : products will not be shipped until payment has been received.') . '</p>';
+		$this->Message = _t('ChequePayment.MESSAGE', 'Payment accepted via Cheque. Please note : products will not be shipped until payment has been received.');
 		
 		$this->write();
 		return new Payment_Success();
@@ -21,7 +21,7 @@ class ChequePayment extends Payment {
 	function getPaymentFormFields() {
 		return new FieldSet(
 			// retrieve cheque content from the ChequeContent() method on this class
-			new LiteralField("Chequeblurb", '<div id="Cheque" class="typography">' . _t('ChequePayment.CONTENT','Please note: Your goods will not be dispatched until we receive your payment.') . '</div>'),
+			new LiteralField("Chequeblurb",  _t('ChequePayment.CONTENT','Please note: Your goods will not be dispatched until we receive your payment.')),
 			new HiddenField("Cheque", "Cheque", 0)
 		);
 	}
